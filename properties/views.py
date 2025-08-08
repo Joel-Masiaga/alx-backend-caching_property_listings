ff739bc67560d9288e62f9e3401f320acfff4783
Joel Masiaga <masiagajoel001@gmail.com>
2025-08-08 06:49:17 +0300

    New commit

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,9 @@
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from django.shortcuts import render
+from .utils import get_all_properties  # will implement next
 
-# Create your views here.
+@cache_page(60 * 15)  # 15 minutes
+def property_list(request):
+    properties = get_all_properties()
+    return render(request, 'properties/property_list.html', {'properties': properties})
